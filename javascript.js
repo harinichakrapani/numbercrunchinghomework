e2507f34907966afbd2fdd82aef6c0a712988f50
harini chakrapani <harini.chakrapani@journalism.cuny.edu>
2016-02-21 19:31:00 -0500

    add all

---
+++ javascript.js
@@ -0,0 +1,33 @@
+function addition(){
+var one= document.getElementById("one").value; 
+var two=document.getElementById("two").value; 
+var addition= one+two; 
+alert("When you add "+ one+ " and "+ two+ " you get "+ addition);  
+}
+
+function subtraction() { 
+
+var one= document.getElementById("one").value; 
+var two=document.getElementById("two").value; 
+var subtraction= one-two; 
+alert("when you subtract "+ one+ " from"+ two+ " you get "+ subtraction);   
+
+}
+
+function multiplication(){ 
+
+var one=document.getElementById("one").value; 
+var two=document.getElementById("two").value; 
+var multiplication= one*two; 
+alert("When you multiply "+ one+ " and "+ two+  " you get "+ multiplication);  
+}
+
+function division(){
+
+var one=document.getElementById("one").value; 
+var two=document.getElementById("two").value; 
+var division= one/two 
+alert(
+	"When you divide"+ one+ " by "+ two+ " you get "+ division); 
+
+}
